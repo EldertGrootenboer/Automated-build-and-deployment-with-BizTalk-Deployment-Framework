2e153e5ea598115e2c780e4085c9de66babd26be
Remco Kapjnga <remco@kapinga.nl>
2020-06-17 20:56:23 +0200

    formatting

--- Deploy/Functions_Install.ps1
+++ Deploy/Functions_Install.ps1
@@ -3,18 +3,16 @@
 
 # Load parameters
 $settings = Import-Csv Settings_DeploymentEnvironment.csv
-foreach($setting in $settings)
-{
+foreach ($setting in $settings) {
     # Program Files directory where application should be installed
-    if($setting.'Name;Value'.Split(";")[0].Trim() -eq "programFilesDirectory") { $programFilesDirectory = $setting.'Name;Value'.Split(";")[1].Trim() }
-    
+    if ($setting.'Name;Value'.Split(";")[0].Trim() -eq "programFilesDirectory") { $programFilesDirectory = $setting.'Name;Value'.Split(";")[1].Trim() }
+
     # Suffix as set in in the ProductName section of the BTDF project file. By default this is " for BizTalk".
-    if($setting.'Name;Value'.Split(";")[0].Trim() -eq "productNameSuffix") { $productNameSuffix = $setting.'Name;Value'.Split(";")[1].TrimEnd() }
+    if ($setting.'Name;Value'.Split(";")[0].Trim() -eq "productNameSuffix") { $productNameSuffix = $setting.'Name;Value'.Split(";")[1].TrimEnd() }
 }
 
 # Install applications
-function InstallBizTalkApplications([string]$msiDirectory)
-{
+function InstallBizTalkApplications([string]$msiDirectory) {
     # Clear log files
     ClearLogFiles $msiDirectory
 
@@ -22,16 +20,14 @@ function InstallBizTalkApplications([string]$msiDirectory)
     $files = GetMsiFiles $msiDirectory
 
     # Loop through MSI files
-    foreach($file in $files)
-    {
+    foreach ($file in $files) {
         # Install application
         InstallBizTalkApplication $file
     }
 }
 
 # Install BizTalk application
-function InstallBizTalkApplication([System.IO.FileInfo]$fileInfo)
-{
+function InstallBizTalkApplication([System.IO.FileInfo]$fileInfo) {
     # Get application name and version
     # We assume msi file name is in the format ApplicationName-Version
     $application = $fileInfo.BaseName.Split("-")[0]
@@ -48,14 +44,13 @@ function InstallBizTalkApplication([System.IO.FileInfo]$fileInfo)
 
     # Install application
     Start-Process -FilePath "msiexec.exe" -ArgumentList "/i ""$msiPath"" /passive /log ""$logFileName"" INSTALLDIR=""$programFilesDirectory\$application$productNameSuffix\$version""" -Wait -Passthru | Out-Null
-    
+
     # Check if installation was successful
-    if((Select-String -Path $logFileName -Pattern "success or error status: 0" -Quiet) -eq "true")
-    {
+    if ((Select-String -Path $logFileName -Pattern "success or error status: 0" -Quiet) -eq "true") {
         Write-Host "$application installed successfully" -ForegroundColor Green
     }
-    else
-    {
+    else {
         Write-Host "$application not installed successfully" -ForegroundColor Red
     }
 }
+
